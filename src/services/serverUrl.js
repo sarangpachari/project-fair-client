e6f17ddb31808a9949b83161b79f87ac65ad9298
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2024-12-12 11:16:17 +0530

    server url updated

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,2 +1,3 @@
-const SERVER_BASE_URL = "http://localhost:3000"
+// const SERVER_BASE_URL = "http://localhost:3000"
+const SERVER_BASE_URL = "https://project-fair-server-6dbn.onrender.com"
 export default SERVER_BASE_URL
